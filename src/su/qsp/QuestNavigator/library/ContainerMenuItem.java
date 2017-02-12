8f9250a3152dba013c0c1dbf08ee0a20ba905660
Nex Otaku <nex@otaku.ru>
2017-02-12 14:00:32 +0300

    Вынес Java-файлы в библиотечный проект.

--- src/su/qsp/QuestNavigator/library/ContainerMenuItem.java
+++
@@ -1,6 +0,0 @@
-package su.qsp.QuestNavigator.library;
-
-public class ContainerMenuItem {
-	String name;
-	String imgPath;
-}
--- src/su/qsp/QuestNavigator/library/ContainerMusic.java
+++
@@ -1,9 +0,0 @@
-package su.qsp.QuestNavigator.library;
-
-import android.media.MediaPlayer;
-
-public class ContainerMusic {
-	String path;
-	int volume;
-	MediaPlayer player;
-}
